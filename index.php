df5b49a142e32c7d4cc6b38ab9bb05bed98c070a
Wellington Ribeiro <devwellington@gmail.com>
2014-08-17 13:04:08 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>DevWellington | Wellington Ribeiro</title>
+</head>
+<body>
+	<header>
+		<h2>DevWellington | Wellington Ribeiro</h2>
+	</header>
+	<p>
+		Estamos trabalhando !!!
+	</p>
+	<footer>
+		<ul>
+			<li><small><strong>eMail: </strong>DevWellington@gmail.com</small></li>
+			<li><small><strong>Twitter: </strong>@DevWellington</small></li>
+			<li><small><strong>GitHub: </strong>https://github.com/DevWellington</small></li>
+		</ul>
+	</footer>
+</body>
+</html>
