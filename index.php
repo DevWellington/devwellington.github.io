976e5bc7c153eda3cec7cd8b717809ccf4e32884
Wellington Ribeiro <devwellington@gmail.com>
2015-03-22 12:48:25 -0300

    Redirect to branded.me/

--- index.php
+++ index.php
@@ -2,20 +2,12 @@
 <html>
 <head>
 	<title>DevWellington | Wellington Ribeiro</title>
+	<META http-equiv="refresh" content="1;URL=http://devwellington.branded.me/"> 
 </head>
 <body>
 	<header>
-		<h2>DevWellington | Wellington Ribeiro</h2>
+		Redirecionando ...
 	</header>
-	<p>
-		Estamos trabalhando !!!
-	</p>
-	<footer>
-		<ul>
-			<li><small><strong>eMail: </strong>DevWellington@gmail.com</small></li>
-			<li><small><strong>Twitter: </strong>@DevWellington</small></li>
-			<li><small><strong>GitHub: </strong>https://github.com/DevWellington</small></li>
-		</ul>
-	</footer>
+		
 </body>
 </html>
